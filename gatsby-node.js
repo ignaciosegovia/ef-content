0ee41e473d471287bf3cb6995bdef5ba60f83626
Angel <asanchez@altimetrik.com>
2020-05-02 17:46:40 -0300

    S3 uploader

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const Promise = require('bluebird')
 const path = require('path')
+var s3 = require('s3')
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
@@ -19,8 +20,8 @@ exports.createPages = ({ graphql, actions }) => {
               }
             }
           }
-          `
-      ).then(result => {
+        `
+      ).then((result) => {
         if (result.errors) {
           console.log(result.errors)
           reject(result.errors)
@@ -32,11 +33,65 @@ exports.createPages = ({ graphql, actions }) => {
             path: `/blog/${post.node.slug}/`,
             component: blogPost,
             context: {
-              slug: post.node.slug
+              slug: post.node.slug,
             },
           })
         })
       })
     )
+  })
+}
+
+exports.onPostBuild = function (pages) {
+  // perform actions on pages here
+  return new Promise((resolve, reject) => {
+    if (
+      process.env.AWS_ACCESS_KEY_ID &&
+      process.env.AWS_SECRET_ACCESS_KEY &&
+      process.env.AWS_S3_BUCKET
+    ) {
+      // Init S3 client
+      var client = s3.createClient({
+        s3RetryCount: 6,
+        s3RetryDelay: 30,
+        s3Options: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          signatureVersion: 'v4',
+          region: process.env.AWS_REGION,
+        },
+      })
+
+      // Sync dir
+      var params = {
+        localDir: 'public',
+        deleteRemoved: true,
+        s3Params: {
+          Bucket: process.env.AWS_S3_BUCKET,
+          Prefix: process.env.AWS_S3_BUCKET_PREFIX,
+        },
+      }
+      var uploader = client.uploadDir(params)
+      console.log(`Start uploading to S3`)
+      uploader.on('error', function (err) {
+        console.error('unable to sync:', err.stack)
+        reject(err)
+      })
+      uploader.on('progress', function (p) {
+        console.log(
+          `Uploading status: ${
+            (parseInt(uploader.progressAmount) /
+              parseInt(uploader.progressTotal)) *
+            100
+          }% (${uploader.progressAmount} of ${uploader.progressTotal} files)`
+        )
+      })
+      uploader.on('end', function () {
+        console.log('done uploading')
+        resolve()
+      })
+    } else {
+      resolve()
+    }
   })
 }
