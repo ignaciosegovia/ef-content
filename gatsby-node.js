f75acf28345503c42c38fe14c7442c65051292be
Angel <asanchez@altimetrik.com>
2020-05-19 18:01:13 -0300

    Remove comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,12 @@
-const Promise = require('bluebird')
-const path = require('path')
-var s3 = require('s3')
+const Promise = require("bluebird");
+const path = require("path");
+var s3 = require("s3");
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
+    const blogPost = path.resolve("./src/templates/blog-post.js");
     graphql(
       `
         {
@@ -23,11 +23,11 @@ exports.createPages = ({ graphql, actions }) => {
     )
       .then((result) => {
         if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
+          console.log(result.errors);
+          reject(result.errors);
         }
 
-        const posts = result.data.allContentfulBlogPost.edges
+        const posts = result.data.allContentfulBlogPost.edges;
 
         posts.forEach((post, index) => {
           const a = createPage({
@@ -36,19 +36,18 @@ exports.createPages = ({ graphql, actions }) => {
             context: {
               slug: post.node.slug,
             },
-          })
-        })
-        resolve()
+          });
+        });
+        resolve();
       })
       .catch((e) => {
-        console.log(e)
-        reject(e)
-      })
-  })
-}
+        console.log(e);
+        reject(e);
+      });
+  });
+};
 
 exports.onPostBuild = function (pages) {
-  // perform actions on pages here
   return new Promise((resolve, reject) => {
     if (
       process.env.AWS_ACCESS_KEY_ID &&
@@ -62,44 +61,44 @@ exports.onPostBuild = function (pages) {
         s3Options: {
           accessKeyId: process.env.AWS_ACCESS_KEY_ID,
           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          signatureVersion: 'v4',
+          signatureVersion: "v4",
           region: process.env.AWS_REGION,
         },
-      })
+      });
 
       // Sync dir
       var params = {
-        localDir: 'public',
+        localDir: "public",
         deleteRemoved: true,
         s3Params: {
           Bucket: process.env.AWS_S3_BUCKET,
           Prefix: process.env.AWS_S3_BUCKET_PREFIX,
         },
-      }
-      var uploader = client.uploadDir(params)
-      uploader.on('error', function (err) {
-        console.error('unable to sync:', err.stack)
-        reject(err)
-      })
-      uploader.on('progress', function (p) {
+      };
+      var uploader = client.uploadDir(params);
+      uploader.on("error", function (err) {
+        console.error("unable to sync:", err.stack);
+        reject(err);
+      });
+      uploader.on("progress", function (p) {
         const iterator = parseInt(
           (parseInt(uploader.progressMd5Amount) /
             parseInt(uploader.progressMd5Total)) *
             100
-        )
-        const dots = '='.repeat(iterator)
-        const left = 100 - (iterator > 0 ? iterator : 0)
-        const empty = ' '.repeat(left)
+        );
+        const dots = "=".repeat(iterator);
+        const left = 100 - (iterator > 0 ? iterator : 0);
+        const empty = " ".repeat(left);
         process.stdout.write(
           `\rUploading constent to S3 [${dots}${empty}] ${iterator}%`
-        )
-      })
-      uploader.on('end', function () {
-        console.log(`Content uploaded to S3 [${'='.repeat(100)}] 100%`)
-        resolve()
-      })
+        );
+      });
+      uploader.on("end", function () {
+        console.log(`Content uploaded to S3 [${"=".repeat(100)}] 100%`);
+        resolve();
+      });
     } else {
-      resolve()
+      resolve();
     }
-  })
-}
+  });
+};
