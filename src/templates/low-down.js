63270127738962bfe4a5ffb85ec3e7f2f092eba3
rodriguezmarting <martin.rg25@hotmail.com>
2020-06-08 18:13:09 -0300

    Style updates.
    Padding for content.
    Transparent backgrounds.
    Reduced space between card and label.
    Label alignment.
    Header animation on scroll.

--- src/templates/low-down.js
+++ src/templates/low-down.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
-import { graphql } from 'gatsby'
+import React, {useState, useRef, useEffect} from 'react'
+import {graphql} from 'gatsby'
 import get from 'lodash/get'
 import Layout from '../components/layout'
 import Dropdown from '../components/dropdown'
-import { isMobile } from 'react-device-detect'
+import {isMobile} from 'react-device-detect'
 import classes from './LowDownNittyGritty.module.css'
 
 const LowDownTemplate = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const lifeEvent = get(props, 'data.allContentfulLifeEvent.nodes[0]');
+  const imgEl = useRef(null);
+
+  const handleScroll = e => {
+    if (e.target.scrollingElement.scrollTop > 50) {
+      imgEl.current.style.height = '180px';
+    } else {
+      imgEl.current.style.height = '490px';
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, [])
 
   return (
     <Layout location={props.location}>
@@ -33,44 +46,44 @@ const LowDownTemplate = (props) => {
             )
           })
         ) : (
-          <div
-            className="wrapper"
-            style={{ padding: '0', margin: '0', width: '100%'}}
-          >
-            <div className={classes.heroContainer}>
-              <img className={classes.heroImg} alt="hero" src={lifeEvent.image.file.url} style={{width: '100%'}} />
-            </div>
-            <div style={{ display: 'flex', marginTop: '3%' }}>
-              {lifeEvent.lowDown.map((le, index) => {
-                return (
-                  <div
-                    onClick={() => {
-                      setCurrentIndex(index);
-                    }}
-                    style={{width: '20%', cursor: 'pointer', marginRight: '2%'}}
-                  >
-                    <img src={le.heroImage? le.heroImage.file.url : '/'} alt="hero" style={{width: '100%'}}/>
-                    <p style={{ color: currentIndex === index ? '#2CCD79' : '#293861', fontWeight: 'bold', margin: 0, textAlign: 'center' }}>
-                      {le.slug
-                        .split('-')
-                        .map(
-                          (word) => word.charAt(0).toUpperCase() + word.slice(1)
-                        )
-                        .join(' ')}
-                    </p>
-                  </div>
-                )
-              })}
-            </div>
             <div
-              dangerouslySetInnerHTML={{
-                __html:
-                  lifeEvent.lowDown[currentIndex].body.childMarkdownRemark
-                    .html,
-              }}
-            />
-          </div>
-        )}
+              className={classes.wrapper}
+              style={{padding: '0', margin: '0', width: '100%'}}
+            >
+              <img ref={imgEl} className={classes.heroImg} alt="hero" src={lifeEvent.image.file.url} />
+              <div className={classes.content}>
+                <div style={{display: 'flex'}}>
+                  {lifeEvent.lowDown.map((le, index) => {
+                    return (
+                      <div
+                        onClick={() => {
+                          setCurrentIndex(index);
+                        }}
+                        style={{width: '20%', cursor: 'pointer', marginRight: '2%'}}
+                      >
+                        <img src={le.heroImage ? le.heroImage.file.url : '/'} alt="hero" style={{width: '100%'}} />
+                        <p style={{color: currentIndex === index ? '#2CCD79' : '#293861', fontWeight: 'bold', margin: '-10px 0 0 18px'}}>
+                          {le.slug
+                            .split('-')
+                            .map(
+                              (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                            )
+                            .join(' ')}
+                        </p>
+                      </div>
+                    )
+                  })}
+                </div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      lifeEvent.lowDown[currentIndex].body.childMarkdownRemark
+                        .html,
+                  }}
+                />
+              </div>
+            </div>
+          )}
       </div>
     </Layout>
   )
--- src/templates/nitty-gritty.js
+++ src/templates/nitty-gritty.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Layout from '../components/layout'
@@ -7,86 +7,99 @@ import { isMobile } from 'react-device-detect'
 import classes from './LowDownNittyGritty.module.css'
 
 const NittyGrittyTemplate = (props) => {
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const lifeEvent = get(props, 'data.allContentfulLifeEvent.nodes[0]')
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const lifeEvent = get(props, 'data.allContentfulLifeEvent.nodes[0]')
+    const imgEl = useRef(null);
+    const contentEl = useRef(null);
 
-  return (
-    <Layout location={props.location}>
-      <div className={classes.container}>
-        {isMobile ? (
-          lifeEvent.nittyGritty.map((data) => {
-            return (
-              <Dropdown
-                title={data.slug
-                  .split('-')
-                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                  .join(' ')}
-              >
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: data.body.childMarkdownRemark.html,
-                  }}
-                />
-              </Dropdown>
-            )
-          })
-        ) : (
-          <div
-            className="wrapper"
-            style={{ padding: '0', margin: '0', width: '100%' }}
-          >
-            <div className={classes.heroContainer}>
-              <img className={classes.heroImg} alt="hero" src={lifeEvent.image.file.url} style={{width: '100%'}} />
-            </div>
-            <div style={{ display: 'flex', marginTop: '3%' }}>
-              {lifeEvent.nittyGritty.map((le, index) => {
-                return (
-                  <div
-                    onClick={() => {
-                      setCurrentIndex(index)
-                    }}
-                    style={{
-                      width: '20%',
-                      cursor: 'pointer',
-                      marginRight: '2%',
-                    }}
-                  >
-                    <img
-                      src={le.heroImage.file.url}
-                      alt="hero"
-                      style={{ width: '100%' }}
-                    />
-                    <p
-                      style={{
-                        color: currentIndex === index ? '#2CCD79' : '#293861',
-                        fontWeight: 'bold',
-                        margin: 0,
-                        textAlign: 'center',
-                      }}
-                    >
-                      {le.slug
-                        .split('-')
-                        .map(
-                          (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    const handleScroll = e => {
+        if (e.target.scrollingElement.scrollTop > 50) {
+            imgEl.current.style.height = '180px';
+        } else {
+            imgEl.current.style.height = '490px';
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, [])
+
+    return (
+        <Layout location={props.location}>
+            <div className={classes.container}>
+                {isMobile ? (
+                    lifeEvent.nittyGritty.map((data) => {
+                        return (
+                            <Dropdown
+                                title={data.slug
+                                    .split('-')
+                                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                                    .join(' ')}
+                            >
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: data.body.childMarkdownRemark.html,
+                                    }}
+                                />
+                            </Dropdown>
                         )
-                        .join(' ').replace("Nitty_gritty", '')}
-                    </p>
-                  </div>
-                )
-              })}
+                    })
+                ) : (
+                        <div
+                            className={classes.wrapper}
+                            style={{ padding: '0', margin: '0', width: '100%' }}
+                        >
+                            <img ref={imgEl} className={classes.heroImg} alt="hero" src={lifeEvent.image.file.url} />
+                            <div ref={contentEl} className={classes.content}>
+                                <div style={{ display: 'flex' }}>
+                                    {lifeEvent.nittyGritty.map((le, index) => {
+                                        return (
+                                            <div
+                                                onClick={() => {
+                                                    setCurrentIndex(index)
+                                                }}
+                                                style={{
+                                                    width: '20%',
+                                                    cursor: 'pointer',
+                                                    marginRight: '2%'
+                                                }}
+                                            >
+                                                <img
+                                                    src={le.heroImage.file.url}
+                                                    alt="hero"
+                                                    style={{ width: '100%' }}
+                                                />
+                                                <p
+                                                    style={{
+                                                        color: currentIndex === index ? '#2CCD79' : '#293861',
+                                                        fontWeight: 'bold',
+                                                        margin: '-10px 0 0 18px',
+                                                    }}
+                                                >
+                                                    {le.slug
+                                                        .split('-')
+                                                        .map(
+                                                            (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                                                        )
+                                                        .join(' ').replace("Nitty_gritty", '')}
+                                                </p>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html:
+                                            lifeEvent.nittyGritty[currentIndex].body.childMarkdownRemark
+                                                .html,
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    )}
             </div>
-            <div
-              dangerouslySetInnerHTML={{
-                __html:
-                  lifeEvent.nittyGritty[currentIndex].body.childMarkdownRemark
-                    .html,
-              }}
-            />
-          </div>
-        )}
-      </div>
-    </Layout>
-  )
+        </Layout>
+    )
 }
 
 export default NittyGrittyTemplate
